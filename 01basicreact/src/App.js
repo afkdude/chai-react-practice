230b2809beb80571dbee6a1d149c1615d81e6847
afkdude <kumarjordy12@gmail.com>
2024-08-08 21:15:27 +0530

    updated

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+   <h1>CHai or React || Shubham amrawat</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+
+function App() {
+
+
+  return (
+   <h1>Chai or react with VITE|shubham</h1>
+  )
+}
+
+export default App
